661e37933d223f1b103ec27c8741a8a06694f2bc
Divya R <kwrdivyaraj100@gmail.com>
2023-10-16 13:48:30 +0530

    Updating the wrong letters

--- script.js
+++ script.js
@@ -35,3 +35,25 @@ function displayWord(){
         popup.style.display= 'flex';
     }
 }
+
+
+function updateWrongLetterE1(){
+    
+    wrongLettersE1.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    
+    figureParts.forEach((part,index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block'
+        }
+        else{
+            part.style.display = 'none';
+        }
+    });
+
+}
