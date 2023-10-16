b532ec7d8573e8ad7e57490e0ee1451f1c6396f5
Divya R <kwrdivyaraj100@gmail.com>
2023-10-16 13:52:48 +0530

    Logic for keydown press letters

--- script.js
+++ script.js
@@ -73,3 +73,27 @@ function showNotification(){
         notification.classList.remove('show');
     }, 2000);
 }
+
+window.addEventListener('keydown', e =>{
+    if(e.keyCode >= 65 && e.keyCode <=90){
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+
+                displayWord();
+            } else{
+                showNotification();
+            }
+        } else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+
+                updateWrongLetterE1();
+            } else{
+                showNotification();
+            }
+        }
+    }
+});
