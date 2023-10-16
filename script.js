9b130043b4a353fd7035c0ca12d3f9bafdd44368
Divya R <kwrdivyaraj100@gmail.com>
2023-10-16 13:41:18 +0530

    Showing hidden word

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 const correctLetters = [];
 const wrongLetters = [];
+
+function displayWord(){
+    wordE1.innerHTML = `
+    ${selectedWord
+    .split('')
+    .map(
+        letter =>`
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+        `
+    )
+    .join('')}
+    `;
+
+    const innerWord = wordE1.innerText.replace(/\n/g, '');
+
+    if(innerWord === selectedWord){
+        finalMessage.innerText = 'Congratulations! You won! 😃';
+        popup.style.display= 'flex';
+    }
+}
